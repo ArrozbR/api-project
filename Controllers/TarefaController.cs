d8d6ccb11ab95bf94c28ac30e2d7099d440afdd5
ArrozbR <pedroblamires@gmail.com>
2025-05-13 14:42:02 -0300

    Desafio Finalizado

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -1,95 +1,80 @@
+using System.ComponentModel;
 using Microsoft.AspNetCore.Mvc;
 using TrilhaApiDesafio.Context;
 using TrilhaApiDesafio.Models;
 
-namespace TrilhaApiDesafio.Controllers
-{
+namespace TrilhaApiDesafio.Controllers{
     [ApiController]
     [Route("[controller]")]
-    public class TarefaController : ControllerBase
-    {
+    public class TarefaController : ControllerBase{
         private readonly OrganizadorContext _context;
 
-        public TarefaController(OrganizadorContext context)
-        {
+        public TarefaController(OrganizadorContext context){
             _context = context;
         }
 
         [HttpGet("{id}")]
-        public IActionResult ObterPorId(int id)
-        {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+        public IActionResult ObterPorId(int id){
+            var tarefabanco = _context.Tarefas.Find(id);
+            if(tarefabanco == null) return NotFound();
+            return Ok(tarefabanco);
         }
 
         [HttpGet("ObterTodos")]
-        public IActionResult ObterTodos()
-        {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+        public IActionResult ObterTodos(){
+            if(!_context.Tarefas.Any()) return NotFound();
+            return Ok(_context.Tarefas);
         }
 
-        [HttpGet("ObterPorTitulo")]
-        public IActionResult ObterPorTitulo(string titulo)
-        {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            return Ok();
-        }
-
-        [HttpGet("ObterPorData")]
-        public IActionResult ObterPorData(DateTime data)
-        {
-            var tarefa = _context.Tarefas.Where(x => x.Data.Date == data.Date);
+        [HttpGet("ObterPorTitulo/{titulo}")]
+        public IActionResult ObterPorTitulo(string titulo){
+            var tarefa = _context.Tarefas.Where(x => x.Titulo.Contains(titulo));
+            if(!tarefa.Any()) return NotFound();
             return Ok(tarefa);
         }
 
-        [HttpGet("ObterPorStatus")]
-        public IActionResult ObterPorStatus(EnumStatusTarefa status)
-        {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
+        [HttpGet("ObterPorData/{data}")]
+        public IActionResult ObterPorData(DateTime data){
+            var tarefa = _context.Tarefas.Where(x => x.Data.Date == data.Date);
+            if(!tarefa.Any()) return NotFound();
+            return Ok(tarefa);
+        }
+
+        [HttpGet("ObterPorStatus/{status}")]
+        public IActionResult ObterPorStatus(EnumStatusTarefa status){
             var tarefa = _context.Tarefas.Where(x => x.Status == status);
+            if(!tarefa.Any()) return NotFound();
             return Ok(tarefa);
         }
 
         [HttpPost]
-        public IActionResult Criar(Tarefa tarefa)
-        {
-            if (tarefa.Data == DateTime.MinValue)
-                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
-
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+        public IActionResult Criar(Tarefa tarefa){
+            if (tarefa.Data == DateTime.MinValue) return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Atualizar(int id, Tarefa tarefa)
-        {
+        public IActionResult Atualizar(int id, Tarefa tarefa){
             var tarefaBanco = _context.Tarefas.Find(id);
-
-            if (tarefaBanco == null)
-                return NotFound();
-
-            if (tarefa.Data == DateTime.MinValue)
-                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
-
-            // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
-            // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            return Ok();
+            if (tarefaBanco == null) return NotFound();
+            if (tarefa.Data == DateTime.MinValue) return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
+            tarefaBanco.Titulo = tarefa.Titulo;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Status = tarefa.Status;
+            _context.Tarefas.Update(tarefaBanco);
+            _context.SaveChanges();
+            return Ok(tarefaBanco);
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Deletar(int id)
-        {
+        public IActionResult Deletar(int id){
             var tarefaBanco = _context.Tarefas.Find(id);
-
-            if (tarefaBanco == null)
-                return NotFound();
-
-            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
+            if (tarefaBanco == null) return NotFound();
+            _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
             return NoContent();
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,33 +1,22 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
-
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OrganizadorContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("StandardConnection")));
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()){
+    app.MapScalarApiReference();
+    app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
+app.UseHttpsRedirection();
 
 app.Run();
